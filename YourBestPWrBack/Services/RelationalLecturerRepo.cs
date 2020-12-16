3d6fec83749befa8356fd128a1fdc1f948a62014
Dund33 <warior1112@gmail.com>
2020-12-17 00:33:37 +0100

    Switched to MySQL. Also made some fields private

--- YourBestPWrBack/Services/RelationalLecturerRepo.cs
+++ YourBestPWrBack/Services/RelationalLecturerRepo.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using MongoDB.Bson;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -11,10 +12,10 @@ namespace YourBestPWrBack.Services
 {
     public class RelationalLecturerRepo : ILecturerRepo
     {
-        const string GetLecturersSQL = "SELECT * FROM LECTURERS;";
-        const string GetOpinionsSQL = "SELECT * FROM OPINIONS;";
-        const string InsertLecturerSQL = "INSERT INTO Lecturers(FirstName, LastName, Title) VALUES (@FirstName, @LastName, @Title);";
-        const string InsertOpinionSQL = "INSERT INTO Opinions(Rating, Description, Date, LecturerId, UserId, CourseId) VALUES (@Rating, @Description, @Date, @LecturerId, @UserId, @CourseId);";
+        private const string GetLecturersSQL = "SELECT * FROM LECTURERS;";
+        private const string GetOpinionsSQL = "SELECT * FROM OPINIONS;";
+        private const string InsertLecturerSQL = "INSERT INTO Lecturers(FirstName, LastName, Title) VALUES (@FirstName, @LastName, @Title);";
+        private const string InsertOpinionSQL = "INSERT INTO Opinions(Rating, Description, Date, LecturerId, UserId, CourseId) VALUES (@Rating, @Description, @Date, @LecturerId, @UserId, @CourseId);";
         private readonly string _connectionString;
 
         public RelationalLecturerRepo(string connectionString)
@@ -24,40 +25,40 @@ namespace YourBestPWrBack.Services
 
         public void AddLecturer(LecturerBasic lecturer)
         {
-            using var connection = new SqlConnection(_connectionString);
+            using var connection = new MySqlConnection(_connectionString);
             connection.Execute(InsertLecturerSQL, lecturer);
         }
 
         public void AddOpinion(Opinion opinion)
         {
-            using var connection = new SqlConnection(_connectionString);
+            using var connection = new MySqlConnection(_connectionString);
             connection.Execute(InsertOpinionSQL, opinion);
         }
 
         public IEnumerable<LecturerBasic> GetLecturers()
         {
-            using var connection = new SqlConnection(_connectionString);
+            using var connection = new MySqlConnection(_connectionString);
             var lecturers = connection.Query<LecturerBasic>(GetLecturersSQL);
             return lecturers;
         }
 
         public async Task<IEnumerable<LecturerBasic>> GetLecturersAsync()
         {
-            using var connection = new SqlConnection(_connectionString);
+            using var connection = new MySqlConnection(_connectionString);
             var lecturers = await connection.QueryAsync<LecturerBasic>(GetLecturersSQL);
             return lecturers;
         }
 
         public IEnumerable<Opinion> GetOpinionsForLecturer(int lecturerId)
         {
-            using var connection = new SqlConnection(_connectionString);
+            using var connection = new MySqlConnection(_connectionString);
             var opinions = connection.Query<Opinion>(GetOpinionsSQL);
             return opinions;
         }
 
         public async Task<IEnumerable<Opinion>> GetOpinionsForLecturerAsync(int lecturerId)
         {
-            using var connection = new SqlConnection(_connectionString);
+            using var connection = new MySqlConnection(_connectionString);
             var opinions = await connection.QueryAsync<Opinion>(GetOpinionsSQL);
             return opinions;
         }
---
+++ YourBestPWrBack/Services/RelationalUserRepo.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using YourBestPWrBack.Models;
+
+namespace YourBestPWrBack.Services
+{
+    public class RelationalUserRepo : IUserRepo
+    {
+        private const string AddUserSQL = "INSERT INTO Users(Username, PasswordHash, GenderId) VALUES (@UserName, @PasswordHash, @GenderId);";
+        private const string GetUserSQL = "SELECT * FROM Users";
+        private readonly string _connectionString;
+        public RelationalUserRepo(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddUser(User user)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            using var transaction = connection.BeginTransaction();
+            var rowsAffected = connection.Execute(AddUserSQL, user);
+            
+            if (rowsAffected != 1)
+                transaction.Rollback();
+            else
+                transaction.Commit();
+        }
+
+        public User GetUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
