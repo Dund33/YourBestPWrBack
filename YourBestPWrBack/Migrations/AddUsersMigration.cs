821ee05cc9b077a30329c2d81e23aa6c0926a6c3
Dund33 <warior1112@gmail.com>
2020-12-19 14:33:33 +0100

    Added FluentMigrations and began implementing migration for setting up Users table

---
+++ YourBestPWrBack/Migrations/AddUsersMigration.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourBestPWrBack.Migrations
+{
+    public class AddUsersMigration : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+        }
+    }
+}
