f1b802f0cf2ab5bd31e53da2e30b61379c6356f9
Dund33 <warior1112@gmail.com>
2021-01-01 21:54:49 +0100

    Made the code a little bit less shitty

--- YourBestPWrBack/Migrations/AddViewsMigration.cs
+++ YourBestPWrBack/Migrations/AddViewsMigration.cs
@@ -6,16 +6,17 @@ namespace YourBestPWrBack.Migrations
     [Migration(3, "AddViews")]
     public class AddViewsMigration: Migration
     {
+        private const string _dbName = "YourBestPWr";
         public override void Up()
         {
-            Execute.Sql(@"create definer = luke@`%` view LecturerAverageWithSubject as
+            Execute.Sql($@"create definer = luke@`%` view LecturerAverageWithSubject as
 select `LC`.`AverageRating`                             AS `AverageRating`,
        `CSS`.`CourseName`                               AS `CourseName`,
-       `YourBestPWr_migrations`.`Lecturers`.`FirstName` AS `FirstName`,
-       `YourBestPWr_migrations`.`Lecturers`.`LastName`  AS `LastName`,
-       `YourBestPWr_migrations`.`Lecturers`.`Title`     AS `Title`
-from ((`YourBestPWr_migrations`.`Lecturers` join `YourBestPWr_migrations`.`LecturersCourses` `LC` on ((`YourBestPWr_migrations`.`Lecturers`.`Id` = `LC`.`LecturerId`)))
-         join `YourBestPWr_migrations`.`Courses` `CSS` on ((`LC`.`CourseId` = `CSS`.`Id`)));");
+       `{_dbName}`.`Lecturers`.`FirstName` AS `FirstName`,
+       `{_dbName}`.`Lecturers`.`LastName`  AS `LastName`,
+       `{_dbName}`.`Lecturers`.`Title`     AS `Title`
+from ((`{_dbName}`.`Lecturers` join `{_dbName}`.`LecturersCourses` `LC` on ((`{_dbName}`.`Lecturers`.`Id` = `LC`.`LecturerId`)))
+         join `{_dbName}`.`Courses` `CSS` on ((`LC`.`CourseId` = `CSS`.`Id`)));");
         }
 
         public override void Down()
